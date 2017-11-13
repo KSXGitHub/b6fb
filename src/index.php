2374e541925b3539e96d8f5dfff67ac8a3c366e1
khai96_ <hvksmr1996@gmail.com>
2017-11-13 20:12:29 +0700

    Create test

--- src/index.php
+++ src/index.php
@@ -1,4 +1,18 @@
 <?php
 require_once __DIR__ . '/controller/index.php';
-echo main();
+$renderer = new Renderer(false);
+$element = HtmlElement::emmet(
+  'level-0#lv0.lv0>level-1a*2+level-1b*3>level-2a*3>level-3',
+  function (array $params) {
+    return array_merge(
+      [
+        'X-DEEP' => $params['deep'],
+      ],
+      $params['at-top'] ? ['TOP'] : ['not top'],
+      $params['at-bottom'] ? ['BOTTOM'] : ['not bottom']
+    );
+  }
+);
+echo $renderer->render($element) . "\n";
+//echo main();
 ?>
